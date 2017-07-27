ee11b6f034093efa5b1760222171f74bfac8731e
nvallepalli <prasanth.vallepalli@gmail.com>
2017-07-27 01:07:58 -0500

    python module

---
+++ cloud_creator/creator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#import sys
+import boto3
+import logging
+logging.basicConfig(
+    filename="log1.log",
+    level=10,
+    format="%(asctime)s:%(levelno)s:%(message)s"
+
+)
+
+client = boto3.client('cloudformation')
+def cf_validation():
+    valuation=client.validate_template(TemplateURL = 'https://s3.amazonaws.com/cc-prasanth/cfsample1.json')
+    logging.debug("Validation log: ", valuation)
+
+def cf_stack_creation():
+    response = client.create_stack(
+    StackName = 'cfsample',
+    TemplateURL= 'https://s3.amazonaws.com/cc-prasanth/cfsample1.json',
+    TimeoutInMinutes = 2,
+    OnFailure='ROLLBACK',)
+    logging.debug("Stack creation Output:", response)
+
+def cf_stack_description():
+    description = client.describe_stacks(StackName='cfsample')
+    logging.debug("stack description:", description)
+
+def main():
+    cf_validation()
+    cf_stack_creation()
+    cf_stack_description()
+
+if __name__ == '__main__':
+    main()
