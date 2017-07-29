e3545eb0482662d119faa32fb7d7193f993e3fed
nvallepalli <prasanth.vallepalli@gmail.com>
2017-07-28 23:51:39 -0500

    updated references

--- c_creator/cf_stack_delete.py
+++ c_creator/cf_stack_delete.py
@@ -1,19 +1,18 @@
 import boto3
 import logging
 client = boto3.client('cloudformation')
-sname='cfsample'
 logging.basicConfig(
     filename="log1.log",
     level=10,
     format="%(asctime)s:%(levelno)s:%(message)s"
 )
 
-def stack_deletetion():
+def stack_deletetion(sname):
     s_remove = client.delete_stack(StackName=sname,)
     logging.debug("stack deletion:", s_remove)
 
 def main():
-    stack_deletetion()
+    stack_deletetion("cfsample")
 
 if __name__ == '__main__':
     main()
--- c_creator/creator.py
+++ c_creator/creator.py
@@ -2,22 +2,30 @@
 #import sys
 import boto3
 import logging
+#import os.path
 logging.basicConfig(
     filename="log1.log",
     level=10,
     format="%(asctime)s:%(levelno)s:%(message)s"
 
 )
-sname = 'cfsample'
-url = 'https://s3.amazonaws.com/cc-prasanth/cfsample1.json'
+#sname = 'cfsample'
+#url = 'https://s3.amazonaws.com/cc-prasanth/cfsample1.json'
 
 client = boto3.client('cloudformation')
-def cf_validation():
-    valuation=client.validate_template(TemplateURL = url)
+s3_client = boto3.client('s3')
+
+def main():
+    cf_validation('https://s3.amazonaws.com/cc-prasanth/cfsample1.json')
+    cf_stack_creation('cfsample','https://s3.amazonaws.com/cc-prasanth/cfsample1.json')
+    cf_stack_description('cfsample')
+
+def cf_validation(url):
+    valuation=client.validate_template(TemplateBody = url)
     logging.debug("Validation log: ", valuation)
 
 
-def cf_stack_creation():
+def cf_stack_creation(sname,url):
     response = client.create_stack(
     StackName = sname,
     TemplateURL= url,
@@ -25,14 +33,11 @@ def cf_stack_creation():
     OnFailure='ROLLBACK',)
     logging.debug("Stack creation Output:", response)
 
-def cf_stack_description():
+def cf_stack_description(sname):
     description = client.describe_stacks(StackName=sname)
     logging.debug("stack description:", description)
 
-def main():
-    cf_validation()
-    cf_stack_creation()
-    cf_stack_description()
+
 
 if __name__ == '__main__':
     main()
