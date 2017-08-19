e14ba9dc04a50bb87c12480ad5e1e8e740c194a7
nvallepalli <prasanth.vallepalli@gmail.com>
2017-08-18 22:29:40 -0500

    initial commit

--- c_creator/creator.py
+++ c_creator/creator.py
@@ -2,40 +2,81 @@
 #import sys
 import boto3
 import logging
-#import os.path
+import os.path
+from jinja2 import Environment, FileSystemLoader, Template
+import yaml
+
 logging.basicConfig(
     filename="log1.log",
     level=10,
     format="%(asctime)s:%(levelno)s:%(message)s"
 
 )
-#sname = 'cfsample'
-#url = 'https://s3.amazonaws.com/cc-prasanth/cfsample1.json'
+
 
 client = boto3.client('cloudformation')
+s3_client = boto3.client("s3")
+
 
 def main():
-    cf_validation('https://s3.amazonaws.com/cc-prasanth/cfsample1.json')
-    cf_stack_creation('cfsample','https://s3.amazonaws.com/cc-prasanth/cfsample1.json')
-    cf_stack_description('cfsample')
 
-def cf_validation(url):
-    valuation=client.validate_template(TemplateURL = url)
+    template_file = _template_create("vpc.json", "properties.yaml")
+    _s3_upload(template_file, "cc-prasanth", "vpc.json")
+    _cf_validation(template_file)
+    cf_stack_creation("cManager_VPC", template_file)
+    _cf_stack_description("cManager_VPC")
+
+
+def _template_create(jinja_template, properties_yaml):
+    cf_template = _get_template(jinja_template)
+    yaml_properties = _get_properties(properties_yaml)
+    return cf_template.render(yaml_properties)
+
+
+def _get_template(jinja_template):
+    template_path = _get_file_path()
+    print(template_path)
+    template_env = Environment(
+            autoescape=False,
+            loader=FileSystemLoader(template_path),
+            trim_blocks=False)
+    return template_env.get_template(jinja_template)
+
+
+def _get_properties(properties_yaml):
+    properties_yaml_path = os.path.join(_get_file_path(), properties_yaml)
+    with open(properties_yaml_path) as f:
+        properties_stream = f.read()
+    return yaml.load(properties_stream)
+
+
+def _get_file_path():
+    path = os.path.dirname(os.path.abspath(__file__))
+    split_path = os.path.split(path)
+    return os.path.join(split_path[0], 'templates')
+
+
+def _s3_upload(path, bucket_name, key_name):
+        s3_client.put_object(Body = path, Bucket = bucket_name, Key = key_name)
+
+
+def _cf_validation(path):
+    valuation=client.validate_template(TemplateBody = path)
     logging.debug("Validation log: ", valuation)
 
 
-def cf_stack_creation(sname,url):
+def cf_stack_creation(sname, path):
     response = client.create_stack(
     StackName = sname,
-    TemplateURL= url,
+    TemplateBody= path,
     TimeoutInMinutes = 2,
     OnFailure='ROLLBACK',)
     logging.debug("Stack creation Output:", response)
 
-def cf_stack_description(sname):
+
+def _cf_stack_description(sname):
     description = client.describe_stacks(StackName=sname)
     logging.debug("stack description:", description)
-
 
 
 if __name__ == '__main__':
